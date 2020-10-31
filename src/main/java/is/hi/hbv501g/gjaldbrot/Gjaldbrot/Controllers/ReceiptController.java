59fe27899c06cbfaa9eb7798ba58dbd4ce66f085
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2020-10-31 17:56:57 +0000

    Nav change to folder pathing

--- src/main/java/is/hi/hbv501g/gjaldbrot/Gjaldbrot/Controllers/ReceiptController.java
+++ src/main/java/is/hi/hbv501g/gjaldbrot/Gjaldbrot/Controllers/ReceiptController.java
@@ -1,8 +1,14 @@
 package is.hi.hbv501g.gjaldbrot.Gjaldbrot.Controllers;
 
+import is.hi.hbv501g.gjaldbrot.Gjaldbrot.Entities.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ReceiptController {
-
+    @RequestMapping(value = "/addReceipt", method = RequestMethod.GET)
+    public String loginGET(User user){
+        return "addReceipt";
+    }
 }
