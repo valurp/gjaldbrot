8e1067c305e0e9f8e133c9508ad5fed5e640278f
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2020-11-01 16:06:33 +0000

    why you do me like that?

--- src/main/java/is/hi/hbv501g/gjaldbrot/Gjaldbrot/Controllers/ReceiptController.java
+++ src/main/java/is/hi/hbv501g/gjaldbrot/Gjaldbrot/Controllers/ReceiptController.java
@@ -3,17 +3,25 @@ package is.hi.hbv501g.gjaldbrot.Gjaldbrot.Controllers;
 import is.hi.hbv501g.gjaldbrot.Gjaldbrot.Entities.Receipt;
 import is.hi.hbv501g.gjaldbrot.Gjaldbrot.Entities.User;
 import is.hi.hbv501g.gjaldbrot.Gjaldbrot.Services.ReceiptService;
+import is.hi.hbv501g.gjaldbrot.Gjaldbrot.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ReceiptController {
     private ReceiptService receiptService;
+
+    @Autowired
+    public ReceiptController(ReceiptService receiptService){
+        this.receiptService = receiptService;
+    }
 
     @RequestMapping(value = "/addReceipt", method = RequestMethod.GET)
     public String addreceiptGET(User user){
