28a8a637f1222e6ce5da93a65e37ccb68f8a4e3c
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2020-11-17 02:09:52 +0000

    I'm stupid, deleteReceipt is complete

--- src/main/java/is/hi/hbv501g/gjaldbrot/Gjaldbrot/Controllers/NavController.java
+++ src/main/java/is/hi/hbv501g/gjaldbrot/Gjaldbrot/Controllers/NavController.java
@@ -4,11 +4,13 @@ import is.hi.hbv501g.gjaldbrot.Gjaldbrot.Entities.Receipt;
 import is.hi.hbv501g.gjaldbrot.Gjaldbrot.Services.ReceiptService;
 import is.hi.hbv501g.gjaldbrot.Gjaldbrot.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class NavController {
     private ReceiptService receiptService;
 
@@ -22,17 +24,13 @@ public class NavController {
         return "savings";
     }
 
-    @RequestMapping(value = "/compare", method = RequestMethod.GET)
-    public String compareGET(Model model){
-        return "compare";
-    }
-
 
     @RequestMapping(value="/deleteReceipt/{id}", method = RequestMethod.GET)
-    public String deleteMovie(@PathVariable("id") long id, Model model){
+    public String deleteReceipt(@PathVariable("id") long id, Model model){
+        System.out.println("TestDelete");
         Receipt receipt = receiptService.findById(id).orElseThrow(()-> new IllegalArgumentException("Invalid Receipt ID"));
         receiptService.delete(receipt);
         model.addAttribute("movies", receiptService.findAll());
-        return "getAllReceipts";
+        return "redirect:/allReceipts";
     }
 }
